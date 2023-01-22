b3e915c8595525b010b0b019afb5cb4ac794a0a7
Gavin Kimpson <gkimpson@gmail.com>
2023-01-22 08:11:34 +0000

    fix logout auth error

--- src/utils/setAuthHeader.js
+++ src/utils/setAuthHeader.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const setAuthHeader = (access_token) => {
     if (access_token) {
         axios.defaults.headers = {
+            Accept: 'application/json',
             Authorization: 'Bearer ' + access_token,
         }
     } else {
